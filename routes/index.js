3249a085dd925c9286189ace31a7934040925036
Viraj Chitnis <chitnisviraj@gmail.com>
2014-10-22 01:27:20 -0400

    Added a nav bar
    The main page now has a nav bar.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'OfficeDB' });
+  res.render('index', { title: 'Accounts - OfficeDB', appname: 'OfficeDB' });
 });
 
 module.exports = router;
