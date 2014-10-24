5be1924a7644890c64b060d9e2764cb9726a58b7
Viraj Chitnis <chitnisviraj@gmail.com>
2014-10-24 00:14:11 -0400

    Table borders can be enabled and disabled
    The table borders can be enabled and disabled via settings, at will.

---
+++ public/javascripts/index.js
@@ -0,0 +1,22 @@
+var tableBorderEnabled;
+
+$(document).ready(function() {
+	$("#enableTableBordersButton").click(function() {
+		enableTableBorders();
+	});
+});
+
+function enableTableBorders() {
+	var tableBorderSettingsButtonHTML = $("#enableTableBordersButton").html();
+	var tableBorderEnabledHTML = "<a role='menuitem' tabindex='-1' href='#'><span class='glyphicon glyphicon-ok'></span>&nbsp;Table borders</a>";
+	if (tableBorderEnabled == true) {
+		tableBorderEnabled = false;
+		$("#accountsTable").removeClass("table-bordered");
+		$("#enableTableBordersButton").html("<a role='menuitem' tabindex='-1' href='#'>Table borders</a>");
+	}
+	else {
+		tableBorderEnabled = true;
+		$("#accountsTable").addClass("table-bordered");
+		$("#enableTableBordersButton").html(tableBorderEnabledHTML);
+	}
+}
